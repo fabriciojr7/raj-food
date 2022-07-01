b89009cfce666c11d1ced17d7b781cb8506ced21
Fabrício Júnior <fabriciojunior77@outlook.com>
2022-07-01 20:22:38 -0300

    mudanca na rota de usuario para cliente

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -6,23 +6,23 @@ class UserService {
   }
 
   async listUsers() {
-    return this.httpClient.get('/usuario');
+    return this.httpClient.get('/cliente');
   }
 
   async getUser(id) {
-    return this.httpClient.getById(`/usuario/${id}`);
+    return this.httpClient.getById(`/cliente/${id}`);
   }
 
   async createUser(users) {
-    return this.httpClient.post('/usuario', users);
+    return this.httpClient.post('/cliente', users);
   }
 
   async updateUser(id, user) {
-    return this.httpClient.update(`/usuario/${id}`, user);
+    return this.httpClient.update(`/cliente/${id}`, user);
   }
 
   async deleteUser(id) {
-    return this.httpClient.delete(`/usuario/${id}`);
+    return this.httpClient.delete(`/cliente/${id}`);
   }
 }
 
